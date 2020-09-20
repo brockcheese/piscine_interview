78d6060fe5d50d9e33e86e08abe7e80cf92dac96
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-09-20 18:16:42 -0500

    Compression

--- d00/ex05/compress.c
+++ d00/ex05/compress.c
@@ -12,105 +12,124 @@
 
 #include "header.h"
 
-size_t hash(char *input) {
-	size_t	ans;
+//creates a hash product from the input
 
-	ans = 0;
+size_t hash(char *input) {
+	size_t	ans; //return value
+
+	ans = 0; //initialize return value
 	for (unsigned long i = 0; i < strlen(input); i++) {
-		ans = (ans + input[i]);
+		ans = (ans + input[i]); //creates hash value
 	}
-	return ans;
+	return ans; //returns hash value
 }
+
+//initializes the hash table given the capacity of the array
 
 struct s_dict *dictInit(int capacity) {
 	struct s_dict	*table;
 	int				i;
 
-	table = malloc(sizeof(struct s_dict));
-	table->capacity = capacity;
+	table = malloc(sizeof(struct s_dict)); //creates hash table
+	table->capacity = capacity; //initializes capacity
+	//allocates memory for items in hash table
 	table->items = malloc(sizeof(struct s_item*) * capacity);
 	i = -1;
-	while (++i < capacity)
+	while (++i < capacity) //initializes hash table to NULL
 		table->items[i] = NULL;
-	return (table);
+	return (table); //returns hash table
 }
 
-int	dictInsert(struct s_dict *dict, char *key, int value) {
-	struct s_item *item;
-	struct s_item *temp;
-	int				ans;
+//inserts an item in the hash table given its key and value
 
-	item = malloc(sizeof(struct s_item));
-	item->key = key;
+int	dictInsert(struct s_dict *dict, char *key, int value) {
+	struct s_item *item; //creates item to insert
+	struct s_item *temp;
+	int				ans; //stores the return value
+
+	item = malloc(sizeof(struct s_item)); //allocates memory for item
+	item->key = key; //initializes item with provided values
 	item->value = value;
 	item->next = NULL;
-	ans = hash(key) % dict->capacity;
-	temp = dict->items[ans];
-	if (temp == NULL)
+	ans = hash(key) % dict->capacity; //creates the hash value of the item
+	temp = dict->items[ans]; //stores the current hashed item
+	if (temp == NULL) //if nothing is stored at hash insert item
 		dict->items[ans] = item;
 	else
 	{
-		while (temp->next != NULL)
+		while (temp->next != NULL) //gets to the end of items at hash
 			temp = temp->next;
-		temp->next = item;
+		temp->next = item; //insert item
 	}
-	return ans;
+	return ans; //return the hash value
 }
+
+//finds an element in the hash table given the key
 
 int dictSearch(struct s_dict *dict, char *key) {
-	struct s_item	*item;
+	struct s_item	*item; //return value stored here
 
+	//finds the items at the hashed value
 	item = dict->items[hash(key) % dict->capacity];
-	while (item != NULL) {
+	while (item != NULL) { //searches every item at the hashed key
 		if (strcmp(item->key, key) == 0)
-			return item->value;
-		item = item->next;
+			return item->value; //if names match return item
+		item = item->next; //else check the next item at the hashed key
 	}
-	return (-1);
+	return (-1); //if nothing is found return error
 }
 
+//finds the longest word in a dictionary
+
 int		getmaxlen(struct s_dict *dict, char **header) {
-	int maxlen;
+	int maxlen; //return value
 	int tempmax;
 	struct s_item *temp;
 
-	maxlen = 3;
-	for (int i = 0; i < dict->capacity; i++) {
-		temp = dict->items[i];
-		while(temp != NULL) {
-			tempmax = strlen(temp->key);
+	maxlen = 3; //sets minimum length of a word to 3
+	for (int i = 0; i < dict->capacity; i++) { //for each item in dict
+		temp = dict->items[i]; //checks each word in dict
+		while(temp != NULL) { 
+			tempmax = strlen(temp->key); //potential longest word
 			if (tempmax > maxlen)
-				maxlen = tempmax;
+				maxlen = tempmax; //set return value to new val
+			//adds word to header
 			header[temp->value - 1] = temp->key;
 			temp = temp->next;
 		}
 	}
-	return(maxlen);
+	return(maxlen); //returns the maximum length
 }
+
+//returns the starting hash for the rolling hash
 
 int		gettesthash(int maxlen, int i, char *book) {
-	int		testhash;
+	int		testhash; //return value
 
-	testhash = 0;
+	testhash = 0; //initializes return value
 	for (int j = 0; j < maxlen; j++) {
-		testhash += (int) book[i + j];
+		testhash += (int) book[i + j]; //very basic hashing algorithm
 	}
-	return testhash;
+	return testhash; //return value
 }
 
-int		hashcompare(char *book, int i, char *str) {
-	int		j;
+//compares the needle to the word in the haystack
 
-	j = -1;
-	while(str[++j] && str[j] == book[i + j])
+int		hashcompare(char *book, int i, char *str) {
+	int		j; //index
+
+	j = -1; //initialize the index
+	while(str[++j] && str[j] == book[i + j]) //compares letter by letter
 		;
-	if (!str[j])
+	if (!str[j]) //if they are the same return 0
 		return 0;
 	return -1;
 }
 
+//Compresses a book using a dictionary of most used words
+
 char	*compress(char *book, struct s_dict *dict) {
-	char			*header[256];
+	char			*header[256]; //header containing frequent words
 	char			*newbook;
 	char			*newbookOfficial;
 	int				maxlen;
@@ -124,77 +143,82 @@ char	*compress(char *book, struct s_dict *dict) {
 	int				testhash;
 
 	i = -1;
-	while(++i < 256)
+	while(++i < 256) //initialize header
 		header[i] = NULL;
-	maxlen = getmaxlen(dict, header);
-	newbook = malloc(strlen(book) + 1);
-	newbooki = 0;
-	booklen = strlen(book);
-	testhash = gettesthash(maxlen, 0, book);
-	hashlen = maxlen;
+	maxlen = getmaxlen(dict, header); //gets length of longest word in dict
+	newbook = malloc(strlen(book) + 1); //creates new book to copy into
+	newbooki = 0; //index for the new book
+	booklen = strlen(book); //stores the length of the original book
+	testhash = gettesthash(maxlen, 0, book); //get starting hash value
+	hashlen = maxlen; //var that stores length of current hash value
+	//checks for hash value at each hash length
 	for(i = 0; i < booklen - 3; i++) {
-		check = 0;
+		check = 0; //initializes a check if hash is found in book
 		for (int j = maxlen - 1; j > 2; j--) {
-			if (testhash >= 0)
+			if (testhash >= 0) //sets temp to value in hash table
 				temp = dict->items[testhash % dict->capacity];
 			else
 				temp = NULL;
 			while (temp != NULL) {
+				//compares hash and replaces with @ symbol
 				if (hashcompare(book, i, temp->key) == 0) {
 					newbook[newbooki++] = '@';
 					newbook[newbooki++] = temp->value;
-					check = 1;
-					i += j;
-					temp = NULL;
+					check = 1; //sets check to checked
+					i += j; //increments index by subindex
+					temp = NULL; //resets temp value
 				}
-				else
+				else //checks next item in hash
 					temp = temp->next;
 			}
-			if (check == 1)
+			if (check == 1) //escapes inner for loop if hash found
 				break;
-			testhash -= book[i + j];
-			hashlen--;
+			testhash -= book[i + j]; //subs last letter from hash
+			hashlen--; //decreases length of hash by one
 		}
 		if (i < booklen - 3) {
-			if (check == 1) {
+			if (check == 1) { //if matching hash found reset hash
 				testhash = gettesthash(maxlen, i + 1, book);
 			}
-			else {
+			else { //continues copying letters to new book
 				newbook[newbooki++] = book[i];
 					//h = 1;
 					//for(int j = 0; j < hashlen - 1; j++) {
 					//	h = (h * 256) % SIZE_MAX;
 					//}
+				//removes first letter from hash
 				testhash -= book[i];
+				//adds last letter until len = maxlen
 				for(int k = hashlen; k <= maxlen; k++) {
 					testhash += book[i + k];
 				}
 			}
-			hashlen = maxlen;
+			hashlen = maxlen; //resets hash to longest word
 		}
 	}
-	while(book[i]) {
+	while(book[i]) { //fills new book with remaining characters from book
 		newbook[newbooki++] = book[i++];
 	}
-	newbook[newbooki] = '\0';
-	i = -1;
-	booklen = 1;
-	while(header[++i])
+	newbook[newbooki] = '\0'; //null character at the end
+	i = -1; //resets index
+	booklen = 1; //resets booklen to calculate the new book
+	while(header[++i]) //adds the header to length for appending later
 		booklen += strlen(header[i]) + 1;
+	//allocates memory for the actual compressed book
 	newbookOfficial = malloc(sizeof(char) * (newbooki + 1 + booklen));
 	newbookOfficial[0] = '<';
-	i = -1;
-	h = 0;
-	while(header[++i]) {
+	i = -1; //resets index
+	h = 0; //index for the compressed book
+	while(header[++i]) { //appends information from the header to book
 		for(int j = 0; header[i][j]; j++)
 			newbookOfficial[++h] = header[i][j];
 		newbookOfficial[++h] = ',';
 	}
 	newbookOfficial[h++] = '>';
-	newbooki = 0;
-	while(newbook[newbooki])
+	newbooki = 0; //initializes index of newbook
+	while(newbook[newbooki]) //appends new book data into compressed book
 		newbookOfficial[h++] = newbook[newbooki++];
-	newbookOfficial[h] = '\0';
-	free(newbook);
-	return newbookOfficial;
+	newbookOfficial[h] = '\0'; //null terminator
+	free(newbook); //frees previously allocated memory
+	return newbookOfficial; //returns compressed book
 }
