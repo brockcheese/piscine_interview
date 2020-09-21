cba85f3aaddff3bbbd0ff86be0310043ef8a0f3d
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-09-21 00:25:45 -0500

    rotate

--- d00/ex07/rotate.c
+++ d00/ex07/rotate.c
@@ -10,15 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rotate(int **picture, int n) {
-	int i, j, temp;
+//rotates an image 90 degrees clockwise
 
-	for(i = 0; i < n/2; i++) {
-		for(j = i; j < n - i - 1; j++) {
-			temp = picture[i][j];
+void	rotate(int **picture, int n) {
+	int i, j, temp; //declare variables
+
+	for(i = 0; i < n/2; i++) { //iterate through each row
+		for(j = i; j < n - i - 1; j++) { //iterate through each column
+			temp = picture[i][j]; //store one pixel to swap
+			//swap top left pixel with bottom left pixel
 			picture[i][j] = picture[n - j - 1][i];
+			//swap bottom left pixel with bottom right pixel
 			picture[n - j - 1][i] = picture[n - i - 1][n - j - 1];
+			//swap bottom right pixel with top right pixel
 			picture[n - i - 1][n - j - 1] = picture[j][n - i - 1];
+			//swap top right pixel with temp
 			picture[j][n - i - 1] = temp;
 		}
 	}
