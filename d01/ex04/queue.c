c7db37e86f921424f8cd1cb26525e357e58c5e6f
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-09-25 21:36:31 -0500

    queue implementation

--- d01/ex04/queue.c
+++ d01/ex04/queue.c
@@ -12,52 +12,62 @@
 
 #include "header.h"
 
-struct s_queue *queueInit(void) {
-	struct s_queue *ans;
+//initializes the queue
 
-	ans = malloc(sizeof(struct s_queue));
-	ans->first = NULL;
-	ans->last = NULL;
-	return (ans);
+struct s_queue *queueInit(void) {
+	struct s_queue *ans; //new queue
+
+	ans = malloc(sizeof(struct s_queue)); //allocates memory for queue
+	ans->first = NULL; //initialize first item in queue
+	ans->last = NULL; //initialize last item in queue
+	return (ans); //return queue
 }
+
+//remove the first item from the queue and return it
 
 char *dequeue(struct s_queue *queue) {
-	struct s_node *item;
-	char *ans;
+	struct s_node *item; //stores first node in queue
+	char *ans; //string to return front of queue
 
-	item = queue->first;
-	if (item == NULL)
+	item = queue->first; //store first node in queue
+	if (item == NULL) //error checking
 		return (NULL);
-	queue->first = item->next;
-	if (queue->first == NULL)
+	queue->first = item->next; //set front of queue to next item
+	if (queue->first == NULL) //sets end of queue to NULL if list is empty
 		queue->last = NULL;
-	ans = item->message;
-	free(item);
-	return (ans);
+	ans = item->message; //sets return value
+	free(item); //frees node
+	return (ans); //return string
 }
+
+//add a message to the end of the queue
 
 void enqueue(struct s_queue *queue, char *message) {
-	struct s_node *newitem;
+	struct s_node *newitem; //node to insert
 
-	newitem = malloc(sizeof(struct s_node));
-	newitem->message = message;
-	newitem->next = NULL;
-	if (queue->last == NULL) {
+	newitem = malloc(sizeof(struct s_node)); //allocates memory for node
+	newitem->message = message; //store message in node
+	newitem->next = NULL; //initialize next to NULL
+	if (queue->last == NULL) { //if queue is empty set item as end of q
 		queue->first = newitem;
 	}
-	else
+	else //attaches node to end of queue
 		queue->last->next = newitem;
-	queue->last = newitem;
+	queue->last = newitem; //sets end of queue as node
 }
+
+//returns the first item of the queue
 
 char *peek(struct s_queue *queue) {
-	if (queue->first == NULL)
+	if (queue->first == NULL) //error check
 		return (NULL);
-	return(queue->first->message);
+	return(queue->first->message); //return the first string in the queue
 }
 
+//returns 1 if the queue is empty, 0 otherwise
+
 int isEmpty(struct s_queue *queue) {
-	if (queue->first == NULL)
+	if (queue->first == NULL) //if queue is empty return 1
 		return (1);
-	return (0);
+	return (0); //return 0 if the queue contains an item
 }
