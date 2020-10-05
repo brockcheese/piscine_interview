556418ff134edd26bfbf6d39d12154291f52d330
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-10-04 19:42:19 -0500

    bubble sort

--- d02/ex00/sortStone.c
+++ d02/ex00/sortStone.c
@@ -12,43 +12,54 @@
 
 #include "header.h"
 
-void sortStones(struct s_stone **stone) {
-	struct s_stone *start;
-	struct s_stone *leftswap;
-	struct s_stone *rightswap;
-	struct s_stone *middleswap;
-	int max = -1;
-	int check = 0;
+//uses a bubble sort to sort a linked list with chunks of the same number
 
-	if (*stone == NULL || (*stone)->next == NULL)
+void sortStones(struct s_stone **stone) {
+	struct s_stone *start; //stores the rest of array to sort
+	struct s_stone *leftswap; //stores leftmost point of chunk
+	struct s_stone *rightswap; //stores rightmost point of chunk
+	struct s_stone *middleswap; //stores middlemost point of chunk
+	int max = -1; //stores the biggest number in the list
+	int check = 0; //checks if the list is sorted
+
+	if (*stone == NULL || (*stone)->next == NULL) //error checking
 		return;
-	start = *stone;
-	while(start) {
+	start = *stone; //initalize start to first node in list
+	while(start) { //find the maximum number in the list
 		max = (start->size > max) ? start->size : max;
 		start = start->next;
 	}
-	max++;
+	max++; //increments largest number
 	while(check == 0) {
-		check = 1;
-		leftswap = *stone;
+		check = 1; //sets check to sorted
+		leftswap = *stone; //sets left pointer to start of list
 		while (leftswap->next && leftswap->size != max) {
+			//sets middle pointer to left pointer
 			middleswap = leftswap;
+			//sets middle pointer to end of chunk
 			while (middleswap->next && middleswap->next->size == leftswap->size)
 				middleswap = middleswap->next;
+			//sets right pointer to middle pointer
 			rightswap = middleswap;
+			//sets right pointer to ened of next chunk
 			while (rightswap->next && rightswap->next->size == middleswap->next->size)
 				rightswap = rightswap->next;
+			//swaps chunks
 			if (leftswap->size > rightswap->size) {
-				check = 0;
+				check = 0; //reset the check
+				//if first chunk is start set start to start of second chunk
 				if (leftswap == (*stone))
 					*stone = middleswap->next;
 				else
 					start->next = middleswap->next;
+				//connects end of first chunk to start of third chunk 
 				middleswap->next = rightswap->next;
+				//connects end of second chunk to start of first chunk
 				rightswap->next = leftswap;
+				//sets new start of list to be end of second chunk
 				start = rightswap;
 			}
-			else {
+			else { //sets new start of list
 				start = middleswap;
 				leftswap = (start && start->next) ? start->next : start;
 			}
