7f06d7abde1562284e1d358aa3adefeac1610467
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-10-09 16:36:03 -0500

    divide and conquer

--- d02/ex07/printYoungest.c
+++ d02/ex07/printYoungest.c
@@ -12,22 +12,29 @@
 
 #include "header.h"
 
+//uses binary search to find youngest person in array
+
 int printYoungestHelper(int *ages, int start, int end) {
 	int m, l, r, max;
 
-	if (end < start)
+	if (end < start) //error checking
 		return (-1);
-	m = (end + start) / 2;
-	max = ages[m];
+	m = (end + start) / 2; //finds middle index
+	max = ages[m]; //initializes int that stores current smallest number
+	//stores smallest number left of m
 	l = printYoungestHelper(ages, start, m - 1);
+	//stores smallest number right of m
 	r = printYoungestHelper(ages, m + 1, end);
 	if (l != -1 && l < max)
-		max = l;
+		max = l; //sets return value as smallest number
 	if (r != -1 && r < max)
-		max = r;
-	return (max);
+		max = r; //sets return value as smallest number
+	return (max); //returns smallest age
 }
 
+//searches through the array using divide and conquer approach
+
 void printYoungest(int *ages, int length) {
+	//prints the youngest age found in int array
 	printf("Youngest : %d\n", printYoungestHelper(ages, 0, length - 1));
 }
