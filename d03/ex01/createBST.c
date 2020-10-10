a3ffd8ee3efc584950f5209b67930b4833bcc0fb
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-10-10 15:22:49 -0500

    array to bst conversion

--- d03/ex01/createBST.c
+++ d03/ex01/createBST.c
@@ -12,18 +12,24 @@
 
 #include "header.h"
 
-struct s_node *createNode(int *arr, int start, int end) {
-	if (start > end)
-		return (NULL);
-	struct s_node *ans;
+//helper function for binary recursion
 
-	int m = (start + end) / 2;
-	ans = malloc(sizeof(struct s_node));
-	ans->value = arr[m];
+struct s_node *createNode(int *arr, int start, int end) {
+	if (start > end) //exit condition
+		return (NULL);
+	struct s_node *ans; //node to be returned
+
+	int m = (start + end) / 2; //stores middle index of array
+	ans = malloc(sizeof(struct s_node)); //creates parent node to return
+	ans->value = arr[m]; //stores value of current index
+	//recursively creates left side of tree
 	ans->left = createNode(arr, start, m - 1);
+	//recursively creates right side of tree
 	ans->right = createNode(arr, m + 1, end);
-	return (ans);
+	return (ans); //returns parent node
 }
+
+//returns a balanced binary search tree given an array of integers
 
 struct s_node *createBST(int *arr, int n) {
 	return (createNode(arr, 0, n - 1));
