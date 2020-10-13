e38692c9f3469a2219264c2313573544b1525c10
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-10-12 22:04:43 -0500

    binary string and or

--- d04/ex01/bit.c
+++ d04/ex01/bit.c
@@ -12,60 +12,70 @@
 
 #include "header.h"
 
+//converts a binary string given as parameter to an integer
+
 int toInt(char *bits) {
-	int ans = 0;
-	int b = 1;
+	int ans = 0; //initializes return value
+	int b = 1; //initializes power of 2
 	for(int i = 3; i >= 0; i--) {
-		ans += (bits[i] - '0') * b;
-		b *= 2;
+		ans += (bits[i] - '0') * b; //add current digit
+		b *= 2; //multiply by base
 	}
-	return (ans);
+	return (ans); //return integer
 }
+
+//returns new string of the product of an and operation between two strings
 
 char *getAnd(char *a, char *b) {
 	int i, j, k, an, bn;
+	//allocates memory for return string
 	char *ans = malloc(sizeof(char) * 5);
-	ans[4] = '\0';
-	i = j = k = an = bn = 0;
-	if (a[0] == '~') {
+	ans[4] = '\0'; //null terminate the string
+	i = j = k = an = bn = 0; //initialize all integer values to 0
+	if (a[0] == '~') { //checks if first string is negative
 		an = 1;
 		i++;
 	}
-	if (b[0] == '~') {
+	if (b[0] == '~') { //checks if second string is negative
 		bn = 1;
 		j++;
 	}
 	while(a[i] && b[j]) {
-		if (an == 1)
+		if (an == 1) //if first string is negative inverse characters
 			a[i] = (a[i] == '1') ? '0' : '1';
-		if (bn == 1)
+		if (bn == 1) //if second string is negative inverse characters
 			b[j] = (b[j] == '1') ? '0' : '1';
+		//calculate and values of current digit
 		ans[k++] = (a[i++] == '1' && b[j] == '1') ? '1' : '0';
-		j++;
+		j++; //increment index
 	}
-	return (ans);
+	return (ans); //return string
 }
+
+//returns new string of the product of an or operation between two strings
 
 char *getOr(char *a, char *b) {
 	int i, j, k, an, bn;
+	//alloates memory for return string
 	char *ans = malloc(sizeof(char) * 5);
-	ans[4] = '\0';
-	i = j = k = an = bn = 0;
-	if (a[0] == '~') {
+	ans[4] = '\0'; //null terminate the string
+	i = j = k = an = bn = 0; //initialize all integer values to 0
+	if (a[0] == '~') { //checks if first string is negative
 		an = 1;
 		i++;
 	}
-	if (b[0] == '~') {
+	if (b[0] == '~') { //checks if second string is negative
 		bn = 1;
 		j++;
 	}
 	while(a[i] && b[j]) {
-		if (an == 1)
+		if (an == 1) //if first string is negative inverse characters
 			a[i] = (a[i] == '1') ? '0' : '1';
-		if (bn == 1)
+		if (bn == 1) //if second string is negative inverse characters
 			b[j] = (b[j] == '1') ? '0' : '1';
+		//calculate or values of current digit
 		ans[k++] = (a[i++] == '1' || b[j] == '1') ? '1' : '0';
-		j++;
+		j++; //increment index
 	}
-	return (ans);
+	return (ans); //return string 
 }
