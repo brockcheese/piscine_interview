396f23a64bcc4df2d9d2cba7b5ab445a013b6420
Armando Sanchez <ArmandoSanchez@Armandos-MBP.cable.rcn.com>
2020-10-13 19:16:23 -0500

    binary string shift

--- d04/ex02/shift.c
+++ d04/ex02/shift.c
@@ -10,38 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//cast a binary string to int
+
 int toInt(char *bits) {
-	int ans = 0;
-	int b = 1;
+	int ans = 0; //initialize return value
+	int b = 1; //initialize power of 2
 	for(int i = 5; i >= 0; i--) {
-		ans += (bits[i] - '0') * b;
-		b *= 2;
+		ans += (bits[i] - '0') * b; //add current digit
+		b *= 2; //multiply by base
 	}
-	if (ans > 31)
+	if (ans > 31) //convert answer to negative
 		ans -= 64;
-	return (ans);
+	return (ans); //return integer
 }
+
+//applies an arithmetical left shift to a binary string bin k shifts
 
 char *leftShift(char *bin, int k) {
-	int i;
+	int i; //index declaration
 
-	for(i = 0; i < 6 - k; i++) {
+	for(i = 0; i < 6 - k; i++) { //move digits k places left
 		bin[i] = bin[i + k];
 	}
-	while(i < 6)
+	while(i < 6) //replace remaining digits with unset bits
 		bin[i++] = '0';
-	return (bin);
+	return (bin); //return shifted binary string
 }
 
-char *rightShift(char *bin, int k) {
-	int i;
-	int sign;
+//applies an arithmetical right shift to a binary string bin k shifts
 
-	sign = bin[0];
+char *rightShift(char *bin, int k) {
+	int i; //index declaration
+	int sign; //integer that stores the sign of the binary string
+
+	sign = bin[0]; //stores the sign of the binary string
 	for(i = 5; i >= k; i--) {
-		bin[i] = bin[i - k];
+		bin[i] = bin[i - k]; //move digits k places right
 	}
-	while (i > -1)
+	while (i > -1) //replace remaining digits with the sign
 		bin[i--] = sign;
-	return (bin);
+	return (bin); //return shifted binary string
 }
